9a7a04140d133339be6f506adfde5981f6a0354e
INJAELEE99 <32193410@dankook.ac.kr>
2025-11-27 15:31:43 +0900

    feat(admin): ê: admin 수정

--- src/main/java/com/ohgiraffers/timedeal/core/domain/AdminService.java
+++ src/main/java/com/ohgiraffers/timedeal/core/domain/AdminService.java
@@ -4,9 +4,9 @@ import com.ohgiraffers.timedeal.storage.AdminsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AdminsService {
+public class AdminService {
     private final AdminsRepository adminsRepository;
-    public AdminsService(AdminsRepository adminsRepository) {
+    public AdminService(AdminsRepository adminsRepository) {
         this.adminsRepository = adminsRepository;
     }
 }
