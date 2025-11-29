88b7db66563d9cd7bb3aa4c46f4960936f42805f
apeirogon99 <apeirogon99@gmail.com>
2025-11-29 09:28:01 +0900

    chore(project): Swagger 추가 및 예제 작성

--- src/main/java/com/ohgiraffers/timedeal/core/api/controller/v1/response/ExampleResponse.java
+++ src/main/java/com/ohgiraffers/timedeal/core/api/controller/v1/response/ExampleResponse.java
@@ -1,6 +1,15 @@
 package com.ohgiraffers.timedeal.core.api.controller.v1.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record ExampleResponse(List<String> examples) {
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+@Schema(description = "예제 응답")
+public record ExampleResponse(
+        @Schema(description = "예제 리스트", example = "hello-world...", requiredMode = REQUIRED)
+        List<String> examples
+) {
+
 }
