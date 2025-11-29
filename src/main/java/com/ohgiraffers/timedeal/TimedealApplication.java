547bc260c6de42efa51058f1d6a2e22c7f9baa27
apeirogon99 <apeirogon99@gmail.com>
2025-11-29 21:30:47 +0900

    chore(project): 스케줄러 config 분리

--- src/main/java/com/ohgiraffers/timedeal/TimedealApplication.java
+++ src/main/java/com/ohgiraffers/timedeal/TimedealApplication.java
@@ -2,10 +2,7 @@ package com.ohgiraffers.timedeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class TimedealApplication {
 
